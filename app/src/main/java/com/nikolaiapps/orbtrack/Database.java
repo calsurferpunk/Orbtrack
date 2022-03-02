818870bb0462edfe499f11385cf8e9379b0057eb
James Nikolai <calsurferpunk@gmail.com>
2022-03-01 16:40:22 -0800

    Update to latest ISS TLE

--- app/src/main/java/com/nikolaiapps/orbtrack/Database.java
+++ app/src/main/java/com/nikolaiapps/orbtrack/Database.java
@@ -101,9 +101,9 @@ public class Database extends SQLiteOpenHelper
     @SuppressWarnings("SpellCheckingInspection")
     private static abstract class TLELines
     {
-        static final String ISSZarya1 = "1 25544U 98067A   22032.91642690  .00005458  00000-0  10449-3 0  9999";
-        static final String ISSZarya2 = "2 25544  51.6445 286.4831 0006762  87.9247  15.7529 15.49730195324201";
-        static final long ISSZaryaDate = 1643754827784L;
+        static final String ISSZarya1 = "1 25544U 98067A   22060.78090161  .00008312  00000-0  15598-3 0  9997";
+        static final String ISSZarya2 = "2 25544  51.6434 148.4913 0005532 202.0961 303.7626 15.49526075328521";
+        static final long ISSZaryaDate = 1646181417144L;
     }
 
     @SuppressWarnings("SpellCheckingInspection")
@@ -1121,7 +1121,7 @@ public class Database extends SQLiteOpenHelper
     private static final int MAX_INFO_LENGTH = 5000;
     private static final int MAX_LANGUAGE_LENGTH = 10;
 
-    private static final int DB_VERSION = 18;
+    private static final int DB_VERSION = 19;
     private static final String DB_NAME = "OrbTrack.DB";
     private static UpdateStatusType updateStatus = null;
 
